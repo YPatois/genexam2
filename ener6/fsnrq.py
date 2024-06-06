7154d7c87293f596ebed85fd06c2cedac4ff0050
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-06-06 11:16:02 +0200

    Correction done

--- ener6/fsnrq.py
+++ ener6/fsnrq.py
@@ -23,17 +23,36 @@ kelementblock="""
     \\begin{reponses}[o]
       @REPONSES@
     \\end{reponses}
+    @EXPLICATION@
   \\end{question}
 }
 """
 
-def print_one_stuff(n,item,response):
+
+explicationblock="""
+\\explain{Une \\emph{source d'énergie} est un phénomène naturel concret (le Soleil,  du pétrole, etc.) dont l'exploitation fourni de l'énergie aux humains.
+
+Une \\emph{forme d'énergie} est la manière dont l'énergie est disponible. Nous en avons vu six:
+\\begin{itemize}
+\\item \emph{énergie thermique} (ou chaleur), liée à la température d'un objet.
+\\item \\emph{énergie électrique}, liée à la circulation d'un courant électrique.
+\\item \\emph{énergie lumineuse}, liée à la lumière (rayonnement).
+\\item \\emph{énergie chimique}, liée à la matière (molécules).
+\\item \\emph{énergie cinétique}, liée à la vitesse d'un objet.
+\\item \\emph{énergie de pesanteur}, liée à la hauteur d'un objet.
+\\item \\emph{énergie nucélaire}, liée au noyau de l'atome.
+\\end{itemize}
+}
+"""
+
+def print_one_stuff(n,item,response,explication):
     qref="autofsnr"+str(n)
 
     block=kelementblock
     block=block.replace("@QREF@",qref)
     block=block.replace("@ITEM@",item)
     block=block.replace("@REPONSES@",response)
+    block=block.replace("@EXPLICATION@",explication)
 
     print('\n')
     print(block)
@@ -44,13 +63,14 @@ def print_one_stuff(n,item,response):
 def print_fsnrq_questions():
   n=0
   response=""
+  explication=""
   response+=build_response(True,"Une forme d'énergie"+'\n')
   response+=build_response(False,"Une source d'énergie"+'\n')
   response+=build_response(False,"Autre chose"+'\n')
 
   for item in fnr:
     n+=1
-    print_one_stuff(n,item,response)
+    print_one_stuff(n,item,response,explication)
 
   response=""
   response+=build_response(False,"Une forme d'énergie"+'\n')
@@ -60,16 +80,20 @@ def print_fsnrq_questions():
 
   for item in snr:
     n+=1
-    print_one_stuff(n,item,response)
+    print_one_stuff(n,item,response,explication)
 
   response=""
   response+=build_response(False,"Une forme d'énergie"+'\n')
   response+=build_response(False,"Une source d'énergie"+'\n')
   response+=build_response(True,"Autre chose"+'\n')
 
+  n0=n
   for item in anr:
     n+=1
-    print_one_stuff(n,item,response)
+    #if (n-n0==len(anr)):
+    #  explication=explicationblock
+
+    print_one_stuff(n,item,response,explication)
 
 
 
