ddd4cf1c5bded1d18afed6d4f92a4c880fb5114b
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-11-18 17:32:19 +0100

    build_acm_question_from_list()

--- python/myamc.py
+++ python/myamc.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import random
 from mytex import lx,lx1,lx1l,lx2,lx2l,qty,begend,begend2
 
-def build_question(elementname,qref,question, reponses):
+def choose3fromlist(l):
+    random.shuffle(l)
+    return(l[:3])
+
+def build_acm_question(elementname,qref,question, reponses):
     # Last is good one
     rs=""
     for r in reponses[:-1]:
@@ -15,6 +20,28 @@ def build_question(elementname,qref,question, reponses):
         ),4
     )
     return(q)
+
+def build_acm_question_from_list(elementname,qref,qlist,nb,
+        qformat,aformat,reverse=False):
+    locallist=qlist.copy()
+    bonne=locallist[nb]
+    # Remove the bonne from the list
+    locallist.remove(bonne)
+    mauvais=choose3fromlist(locallist)
+    rssl=[]
+    if not reverse:
+        idxq=0
+        idxr=1
+    else:
+        idxq=1
+        idxr=0
+    
+    qss=qformat.format(q=bonne[idxq])
+    for i in range(len(mauvais)):
+        rssl.append(aformat.format(a=mauvais[i][idxr]))
+    rssl.append(aformat.format(a=bonne[idxr]))
+    #print (qss,rssl)  
+    return(build_acm_question(elementname,qref,qss,rssl))
 
 # --------------------------------------------------------------------------
 # Welcome to Derry, Maine
