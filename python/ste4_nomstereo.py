e662e14a26fcfc7d5cd7a3b0255ef5cd19811d44
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-11-20 18:40:15 +0100

    works

--- python/ste4_nomstereo.py
+++ python/ste4_nomstereo.py
@@ -4,65 +4,24 @@ import argparse
 import unittest
 import random
 
-from mytex import lx,lx1,lx2,qty
-from myamc import build_acm_question_from_list
+from myacmhelper import build_all_groups_questions
 
-molecule_table = [
-    ['4-éthylheptane'             , '-[:30]-[:90](-[:30]-[:330]-[:30])-[:150]-[:210]-[:150]'],
-    ['2,3-diméthylpentane'        , '-[:90](-[:30](-[:90])-[:330])-[:150]-[:210]'],
-    ['3,4-diméthylhexane'         , '-[:90](-[:150]-[:210])-[:30](-[:90])-[:330]-[:30]'],
-    ['butan-2-amine'              , 'H_2N-[:330,,2](-[:270])-[:30]-[:330]'],
-    ['2-méthylbutan-1-ol'         , 'OH-[:150,,1]-[:210](-[:270])-[:150]-[:210]'],
-    ['3-méthylheptane'            , '-[:90](-[:150]-[:210])-[:30]-[:330]-[:30]-[:330]'],
-    ['2,3-diméthylpentanal'       , 'O=[:210]-[:150](-[:90])-[:210](-[:270])-[:150]-[:210]'],
-    ['3-éthyl-4-méthylhexan-2-one', 'O=[:270](-[:210])-[:330](-[:270]-[:210])-[:30](-[:90])-[:330]-[:30]'],
-    ['Acide 2-méthylpropanoïque'  , 'OH-[:150,,1](=[:90]O)-[:210](-[:270])-[:150]']
+molecule_table = [ 
+    ['acide (2S)-2-aminopropanoïque', 'acide (2R)-2-aminopropanoïque', 
+    '*acide (2S,3R)-2-amino-3-methylpropanoïque', 'acide (2S)-2-amino-3-methylbutanoïque',
+    '*acide (3R)-3-amino-2-methylpropanoïque', '*acide (2S,4R)-2-amino-4-ethylpentanoïque'],
+    ['(2S,3R)-2,3,4-trihydroxybutanal', '(2R,3S)-2,3,4-trihydroxybutanal', '(2S,3S)-2,3,4-trihydroxybutanal',
+    '(2R,3R)-2,3,4-trihydroxybutanal', '*(2S,3R)-2,3,5-trihydroxypentanal', '*(2S,4R)-2,4-dihydroxy-3-oxobutanal'],
+    ['(R)-butan-2-ol', '(S)-butan-2-ol'],
+    ['(R)-bromochlorofluoromethane', '(S)-bromochlorofluoromethane'],
 ]
-
-
-[ 'acide (S,S)-2-aminopropanoïque',  '5950']
-[ 'acide (R,S)-2-aminopropanoïque',   '602']
-[ 'acide (R,R)-2-aminopropanoïque', '71080']
-
-
-
-# mol2chemfig -w -z -y delete -i pubchem 142755607
-
-
-
-
-def massage_table(qlist):
-    newqlist=[]
-    for item in qlist:
-        newqlist.append([item[0],'{'+lx('small')+lx1('chemfig',item[1])+'}'])
-    return(newqlist)
-
-def build_one_group_question(elementname,qref,qlist,nb,reverse=False):
-    print("Building question "+str(nb))
-    if not reverse:
-        qformat="Quel est la formule topologique de: {q}~?\n"
-        aformat="{a}"
-    else:
-        qformat="Quel est le nom de cette molécule: ~?\n \\\\ {q}"
-        aformat="{a}"
-    return build_acm_question_from_list(elementname,qref,qlist,nb,
-        qformat,aformat,reverse)
-
-def build_all_groups_questions():
-    qlist=massage_table(molecule_table)
-    elementbase="nomstereo"
-    q=""
-    for i in range(len(qlist)):
-        q+=build_one_group_question(elementbase,elementbase+str(i),qlist,i)
-        q+=build_one_group_question(elementbase,elementbase+'r'+str(i),qlist,i,reverse=True)
-    return(q)
 
 # --------------------------------------------------------------------------
 # Welcome to Derry, Maine
 # --------------------------------------------------------------------------
 def main():
-    q=build_all_groups_questions()
-    f=open("q_nomstereo.tex","w")
+    q=build_all_groups_questions('nomencrs',molecule_table)
+    f=open("q_nomencrs.tex","w")
     f.write(q)
     f.close()
 
