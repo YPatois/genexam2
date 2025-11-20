8df08f32d98954b2e16af418a776ec8a008305fc
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-11-20 14:37:16 +0100

    closer

--- python/ste4_nomenca.py
+++ python/ste4_nomenca.py
@@ -10,7 +10,7 @@ from myamc import build_acm_question_from_list
 from mypubchem import mol2chemfig_from_frenchname
 
 molecule_table = [
-    ['4-éthylheptane','3-éthylheptane','*5-éthylheptane','*2-éthylheptane','4-méthylheptane','4-propylheptane'],
+    ['4-éthylheptane','3-éthylheptane','*5-éthylheptane','*2-éthylheptane','4-méthylheptane','4-propylheptane','2-méthylhexane','3-méthyloctane'],
     ['2,3-diméthylpentane','2,4-diméthylpentane','3,3-diméthylpentane','2-méthylpentane','*2,3-diéthylpentane'],
     ['butan-2-amine', 'butan-1-amine','3-méthylbutan-2-amine','*2-amino-3-éthylbutane', 'pentan-2-amine','2,3-diaminobutane'],
     ['2-méthylbutan-1-ol', '3-méthylbutan-1-ol', '*2-méthylpentan-1-ol', '2-méthylpropan-1-ol', '*3-éthylbutan-1-ol', '*3-pentylbutan-1-ol'],
@@ -47,7 +47,7 @@ def build_one_group_question(elementname,qref,qlist,nb,reverse=False):
 def build_lists_from_one_list(qlist,id):
     goodmoln=qlist[id]
     if goodmoln[0] == '*':
-        return(None)
+        return (None,None)
     goodmolchemfig=mol2chemfig_from_frenchname(goodmoln)
     badmolnl=[]
     badmolchemfigl=[]
@@ -79,15 +79,16 @@ def build_lists_from_one_list(qlist,id):
 def build_questions_dr_from_one_list(elementbase,qlist,id):
     q=""
     (directqlist,reverseqlist)=build_lists_from_one_list(qlist,id)
+    if directqlist == None:
+        return(q)
     q+=build_one_group_question(elementbase,elementbase+str(id),directqlist,0)
     q+=build_one_group_question(elementbase,elementbase+'r'+str(id),reverseqlist,0,reverse=True)
     return(q)
 
-def build_questions_list(qlist):
+def build_questions_list(elementbase,qlist):
     q=""
     for i in range(len(qlist)):
-        q+=build_one_group_question(elementbase,elementbase+str(i),qlist,i)
-        q+=build_one_group_question(elementbase,elementbase+'r'+str(i),qlist,i,reverse=True)
+        q+=bbuild_questions_dr_from_one_list(elementbase,qlist,i)
     return(q)
 
 
@@ -106,7 +107,7 @@ def build_all_groups_questions(qlist,all=False):
 # Welcome to Derry, Maine
 # --------------------------------------------------------------------------
 def main():
-    q=build_questions_dr_from_one_list('nomenca',molecule_table[0],0)
+    q=build_questions_list('nomenca',molecule_table[0])
     print(q)
     return
 
