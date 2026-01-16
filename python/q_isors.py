ed417d3dcb7ae99ebe566922e2db73d14ce03e82
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2026-01-16 11:52:55 +0100

    simplifier

--- python/q_isors.py
+++ python/q_isors.py
@@ -7,13 +7,25 @@ import random
 from myacmhelper import build_all_groups_questions
 
 molecule_table = [ 
-    ['acide (2S)-2-aminopropanoïque', 'acide (2R)-2-aminopropanoïque', 
-    '*acide (2S,3R)-2-amino-3-methylpropanoïque', 'acide (2S)-2-amino-3-methylbutanoïque',
-    '*acide (3R)-3-amino-2-methylpropanoïque', '*acide (2S,4R)-2-amino-4-ethylpentanoïque'],
-    ['(2S,3R)-2,3,4-trihydroxybutanal', '(2R,3S)-2,3,4-trihydroxybutanal', '(2S,3S)-2,3,4-trihydroxybutanal',
-    '(2R,3R)-2,3,4-trihydroxybutanal', '*(2S,3R)-2,3,5-trihydroxypentanal', '*(2S,4R)-2,4-dihydroxy-3-oxobutanal'],
+    [
+        'acide (2S)-2-hydroxypropanoïque',
+        'acide (2R)-2-hydroxypropanoïque',
+        'acide (2S)-2-aminopropanoïque',
+        'acide (2R)-2-aminopropanoïque',
+        'acide (2S)-2-hydroxybutanoïque',
+        'acide (2R)-2-hydroxybutanoïque',
+        'acide (3S)-3-hydroxybutanoïque',
+        'acide (3R)-3-hydroxybutanoïque'
+    ],
     ['(R)-butan-2-ol', '(S)-butan-2-ol','(R)-butan-2-amine', '(S)-butan-2-amine'],
-    ['(R)-bromochlorofluoromethane', '(S)-bromochlorofluoromethane'],
+    [
+        'acide (2S)-2-hydroxypropanoïque',
+        'acide (2R)-2-hydroxypropanoïque',
+        'acide (2S)-2-hydroxybutanoïque',
+        'acide (2R)-2-hydroxybutanoïque',
+        'acide (3S)-3-hydroxybutanoïque',
+        'acide (3R)-3-hydroxybutanoïque'
+    ]
 ]
 
 # --------------------------------------------------------------------------
